ceeb6593eb6158e95b78f606ff816c2286c12b8c
Lilitha27 <lilithakganya@gmail.com>
2019-07-08 09:49:05 +0200

    first update

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//function displays the dresscode for the day
+function displayMessage(){
+    let temperature = document.getElementById("temp").value;
+    console.log(temperature)
+    if (temperature >= 24) {
+        document.getElementById("dresscode").innerHTML = "Vest and shorts";
+    
+    } else if (temperature <= 23 && temperature >= 15){
+        document.getElementById("dresscode").innerHTML = "wear a windbreaker and have a raincoat";
+        
+    } else if (temperature <= 14 && temperature >= -7) {
+        document.getElementById("dresscode").innerHTML = "wear a warm clothes and a coat";
+      
+    } else {
+        document.getElementById("dresscode").innerHTML = "invalid temperature";
+    
+    }
+    }
+    
