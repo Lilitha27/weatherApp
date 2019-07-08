1488e0ceb1acc10375d75f3109d799e9cfba8e35
Lilitha27 <lilithakganya@gmail.com>
2019-07-08 16:30:46 +0200

    insert images

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function displayUserOutput(){
         if (temperature >=24) {
         document.getElementById("text").innerHTML = "Summer time! wear shorts ,flip flops and sunglasses";
 //  Giving the temp is greater than 23 but less than 14
-        } else if (temperature >=23 && temperature <=14) {
+        } else if (temperature <=23 && temperature >=14) {
         document.getElementById("text").innerHTML ="Wear a wind breaker jacket and have a raincoat";
 //  Giving the temp is greater than 13 but less than -7
         } else if (temperature <=13 && temperature >=-7) {
