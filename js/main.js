8a7da84949fecb1c33c9a4bb650b916a7c6e3515
Lilitha27 <lilithakganya@gmail.com>
2019-07-08 16:24:04 +0200

    update js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function displayUserOutput(){
         } else if (temperature >=23 && temperature <=14) {
         document.getElementById("text").innerHTML ="Wear a wind breaker jacket and have a raincoat";
 //  Giving the temp is greater than 13 but less than -7
-        } else if (temperature >=13 && temperature <=-7) {
+        } else if (temperature <=13 && temperature >=-7) {
         document.getElementById("text").innerHTML ="Dress warmly and bring out your shovel cause its snowy";
 //  Giving that if any of the above values aren't entered then its invalid   
         } else {
